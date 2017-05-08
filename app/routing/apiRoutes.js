b6234198f18e47c21a6bea27cd23a0b12be19384
Andrew Wong <andrew.wong@andrew-wong-compy.local>
2017-05-08 18:38:57 -0400

    file

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+module.exports = function(app){
+
+  app.get("/api/friends", function (req, res){
+
+    return res.json()
+
+  });
+
+  app.post("/api/friends", function (req, res){
+
+  });
+
+};
