60c93b71b222aa706573a9be3b4c2d422fde29d7
Andrew Wong <andrew.wong@andrew-wong-compy.local>
2017-05-17 15:37:19 -0400

    index

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,7 +10,7 @@ module.exports = function(app){
   });
 
   app.use(function(req, res){
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
 };
